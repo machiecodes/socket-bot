f2666450f2a5fc505df1b6ff17788e6057e9d2e7
MachieCodes <rickytheracc@gmail.com>
2026-09-12 15:28:57 -0400

    Fix .env
    Didn't realize node didn't auto interpret .env oops

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const { Client, Collection, GatewayIntentBits, REST } = require("discord.js");
 const { loadCommands, deployCommands } = require("./handlers/command-handler.js");
 const loadEvents = require("./handlers/event-handler.js");
 
+require("dotenv").config();
+
 const client = new Client({
     // https://gist.github.com/advaith1/e69bcc1cdd6d0087322734451f15aa2f
     intents: [
